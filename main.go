2fb8e99acc41d8a1b763d2106901e590bdccc2ed
Rajshri Mohan K S <rajshrimohanks@gmail.com>
2019-12-31 08:18:19 +0530

    Writing data to harddrive

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
