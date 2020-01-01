d3212af64e567c1b339341b4b4c38e2d7da1c77a
Rajshri Mohan K S <rajshrimohanks@gmail.com>
2020-01-01 23:10:50 +0530

    Writing useful tests

--- deck_test.go
+++ deck_test.go
@@ -1,2 +1,12 @@
 package main
 
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	deckLength := 52
+
+	if len(d) != deckLength {
+		t.Errorf("Expected deck length of %v, but got %v", deckLength, len(d))
+	}
+}
