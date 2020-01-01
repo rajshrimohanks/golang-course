4bbdc6af88db7a1d5e76ad4f2c8f37bad18eab1e
Rajshri Mohan K S <rajshrimohanks@gmail.com>
2020-01-01 23:17:50 +0530

    Asserting elements in a slice

--- deck_test.go
+++ deck_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestNewDeck(t *testing.T) {
-	d := newDeck()
 	deckLength := 52
+	firstCard := "Ace of Spades"
+	lastCard := "King of Clubs"
+
+	d := newDeck()
 
 	if len(d) != deckLength {
 		t.Errorf("Expected deck length of %v, but got %v", deckLength, len(d))
+	}
+
+	if d[0] != firstCard {
+		t.Errorf("Expected first card of '%v', but got %v", firstCard, d[0])
+	}
+
+	if d[len(d)-1] != lastCard {
+		t.Errorf("Expected last card of '%v', but got %v", lastCard, d[len(d)-1])
 	}
 }
